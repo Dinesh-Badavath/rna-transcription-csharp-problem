21a3843ca155a6ddbf9fd12dd9403da2ddf42d02
dinesh.badavath <dinesh.badavath@cgi.com>
2019-08-29 14:57:52 +0530

    Solved

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Text;
+using System.Collections.Generic;
 namespace RNATranscriptionModule
 {
-    public static class RnaTranscription
-    {
-        public static string ToRna(string nucleotide)
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+   public static class RnaTranscription
+   {
+               static void Main(string[] args)
+               {}
+   static readonly IReadOnlyDictionary<char, char> dna = new Dictionary<char, char>
+   {
+       ['G'] = 'C',
+       ['C'] = 'G',
+       ['T'] = 'A',
+       ['A'] = 'U',
+   };
+   public static string ToRna(string nucleotide){
+           StringBuilder rna = new StringBuilder(nucleotide);
+             for(int i=0;i<nucleotide.Length;i++){
+                   rna[i] = dna[rna[i]];
+             }
+             return rna.ToString();
+   }
+   }
 }
